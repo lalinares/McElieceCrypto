a65abacc51fefced0875d8b4ac75b1267e5b3c19
LUIS ALFREDO PEREZ LINARES <lperez@prixmasol.com>
2019-10-11 16:36:32 -0500

    CASI LISTO (FALTA EL OUTPUT FORMAL DE LAS LLAVES PK Y SK)

--- src/main/java/com/crypto/uninorte/mceliececrypto/McElieceKem.java
+++ src/main/java/com/crypto/uninorte/mceliececrypto/McElieceKem.java
@@ -13,232 +13,225 @@ import java.util.Arrays;
  * @author pc
  */
 public class McElieceKem {
-    
+
     private int n;
     private int m;
     private int t;
     private int q;
     private int k;
+    private GF2mField field;
     private int fieldPoly;
+    private PolynomialGF2mSmallM goppaFieldPoly;
     private int[] goppaPoly;
     private int[] L;
-    
-    private boolean initialized;
-    
+
     private SecureRandom secureRandom;
-    
-    public McElieceKem(int n, int m, int t){
-        if(t<2){
+
+    public McElieceKem(int n, int m, int t) {
+        if (t < 2) {
             System.err.println("El parámetro t debe ser mayor o igual a 2!");
             return;
         }
-        if(m*t >= n){
+        if (m * t >= n) {
             System.err.println("La condición mt < n no se cumple!");
             return;
         }
-        if(n > 1<<m){
+        if (n > 1 << m) {
             System.err.println("El parámetro n no cumple la condición n <= 2^m");
             return;
         }
-        
+
+        this.secureRandom = new SecureRandom();
+
         this.m = m;
         this.n = n;
         this.t = t;
-        this.q = 1<<m;
-        this.k = this.n - this.m*this.t;
-        
-        this.fieldPoly = generateFieldPoly(this.m);
-        
-        this.secureRandom = new SecureRandom();
-        
+        this.q = 1 << this.m;
+        this.k = this.n - this.m * this.t;
+
+        this.field = new GF2mField(this.m, this.secureRandom);
+
+        this.fieldPoly = this.field.getPolynomial();
+
+        this.goppaFieldPoly = new PolynomialGF2mSmallM(field, t, PolynomialGF2mSmallM.RANDOM_IRREDUCIBLE_POLYNOMIAL, this.secureRandom);
+
+        this.goppaPoly = this.goppaFieldPoly.getCoefficients();
+
+        this.L = generateRandomL();
+
     }
-    
-    public McElieceKem(int[] L, int fieldPoly, int[] goppaPoly){
+
+    public McElieceKem(int[] L, int fieldPoly, int[] goppaPoly) {
         this.L = L;
         this.fieldPoly = fieldPoly;
         this.goppaPoly = goppaPoly;
-        
-        this.n = L.length;
-        this.m = polyDegree(fieldPoly);
-        this.t = goppaPoly.length-1;
-        this.q = 1<<this.m;
+
+        this.secureRandom = new SecureRandom();
+
+        this.m = PolynomialRingGF2.degree(fieldPoly);
+
+        this.field = new GF2mField(this.m, fieldPoly, this.secureRandom);
+
+        this.goppaFieldPoly = new PolynomialGF2mSmallM(field, goppaPoly);
+
+        this.n = this.L.length;
+        this.t = this.goppaPoly.length - 1;
+        this.q = 1 << this.m;
         this.k = this.n - this.m * this.t;
-        
-        if(this.t<2){
+
+        if (this.t < 2) {
             System.err.println("El parámetro t debe ser mayor o igual a 2!");
             return;
         }
-        if(this.m*this.t >= this.n){
+        if (this.m * this.t >= this.n) {
             System.err.println("La condición mt < n no se cumple!");
             return;
         }
-        if(this.n > this.q){
+        if (this.n > this.q) {
             System.err.println("El parámetro n no cumple la condición n <= 2^m");
             return;
         }
-        
-        this.initialized = true;
-    }
-    
-    public boolean isAlphaPrimitiveElement(int poly){
-        boolean[] elements = new boolean[1<<polyDegree(poly)];
-        elements[0] = true;
-        int temp;
-        for (int i = 0; i < elements.length-1; i++) {
-            temp = mod(1<<(i), poly);
-            if(elements[temp]){
-                return false;
-            }
-            elements[temp] = true;
-        }
-        return true;
-    }
-    
-    public int generateFieldPoly(int m){
-        if (m < 0)
-        {
-            System.err.println("The Degree is negative");
-            return 0;
-        }
-        if (m > 31)
-        {
-            System.err.println("The Degree is more than 31");
-            return 0;
-        }
-        if (m == 0)
-        {
-            return 1;
-        }
-        
-        int a = (1<<m) + 1;
-        int b = 1<<(m+1);
-        for(int i = a; i<b;i++){
-            if(isIrreducible(i) && isAlphaPrimitiveElement(i)){
-                return i;
-            }
-        }
-        return 0;
-    }
-    
-    public boolean isIrreducible(int poly){
-        if (poly == 0)
-        {
-            return false;
-        }
-        int d = polyDegree(poly) >>> 1;
-        int u = 2;
-        for (int i = 0; i < d; i++)
-        {
-            u = mod(multiply(u, u), poly);
-            if (gcd(u ^ 2, poly) != 1)
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-    
-    public int gcd(int p, int q)
-    {
-        int a, b, c;
-        a = p;
-        b = q;
-        while (b != 0)
-        {
-            c = mod(a, b);
-            a = b;
-            b = c;
 
-        }
-        return a;
     }
-    
-    public int multiply(int a, int b){
-        int result = 0;
-        while(b != 0){
-            if((b & 0b1) == 1){
-                result = result ^ a;
+
+    public int[] generateRandomL() {
+        boolean[] flags = new boolean[this.q];
+        int[] randomL = new int[this.n];
+        int count = 0;
+        int roots = 0;
+        while (count < this.n && count + roots < this.q) {
+            int element = field.getRandomElement();
+            if (flags[element]) {
+                continue;
             }
-            b >>>=1;
-            a <<=1;
+            if (this.goppaFieldPoly.evaluateAt(element) == 0) {
+                roots++;
+            } else {
+                randomL[count] = element;
+                count++;
+            }
+            flags[element] = true;
         }
-        return result;
-    }
-    
-    public int polyDegree(int poly){
-        int count = -1;
-        while(poly != 0){
-            poly >>>= 1;
-            count++;
+        if (count == this.n) {
+            return randomL;
+        } else {
+            return null;
         }
-        
-        return count;
     }
-    
-    public int mod(int a, int b){
-        int da = polyDegree(a);
-        int db = polyDegree(b);
-        while(da>=db){
-            a = (b<<(da-db)) ^ a;
-            da = polyDegree(a);
-            db = polyDegree(b);
-        }
-        return a;
+
+    public int getElementOfH(int j, int i) {
+        int element = PolynomialRingGF2.remainder(L[j], field.getPolynomial());
+        return field.mult(field.exp(element, i), field.inverse(goppaFieldPoly.evaluateAt(element)));
     }
-    
+
     public static void main(String[] args) {
-        //test
-        McElieceKem mck = new McElieceKem(12, 4, 2);
-        System.out.println(mck.generateFieldPoly(3));
-        //test
-//        int h[][] = {
-//            {0,1,0,1,0,0,1,1,0,1,1,0},
-//            {1,1,1,0,0,0,1,0,0,1,0,0},
-//            {0,1,0,0,1,0,1,1,1,0,0,1},
-//            {1,0,1,1,1,0,0,0,0,1,1,1},
-//            {1,0,0,0,0,1,1,1,0,1,1,0},
-//            {1,0,0,1,1,0,0,0,1,0,1,1},
-//            {1,1,1,0,1,0,0,1,1,0,1,0},
-//            {1,1,1,0,1,0,1,0,1,1,1,0}
-//        };
-//        
-//        int n = h.length;
-//        int m = h[0].length;
-//        for(int i = m-n; i < m; i++){
-//            if(h[i-m+n][i] == 0){
-//                for(int j = i-m+n+1; j<n; j++){
-//                    if(h[j][i] == 1){
-//                       for(int k = 0; k < m; k++){
-//                            h[i-m+n][k] = h[i-m+n][k]^h[j][k];
-//                        }
-//                       break;
-//                    }
-//                }
-//            }
-//            
-//            for(int j = i-m+n+1; j < n; j++){
-//                if(h[j][i] == 1){
-//                    for(int k = i; k < m; k++){
-//                        h[j][k] = h[j][k]^h[i-m+n][k];
-//                    }
-//                }
-//            }
-//            
-//        }
-//        
-////        for(int i = n-1; i>=1; i--){
-////            for(int j = i-1; j>=0; j--){
-////                if(h[j][i] == 1){
-////                    for(int k = i; k<m;k++){
-////                        h[j][k] = h[j][k]^h[i][k];
-////                    }
-////                }
-////            }
-////        }
-//        
-//        
-//        for(int i = 0; i<n; i++){
-//            System.out.println(Arrays.toString(h[i]));
-//        }
+
+//        McElieceKem mck = new McElieceKem(new int[]{
+//            1<<2,
+//            1<<3,
+//            1<<4,
+//            1<<5,
+//            1<<6,
+//            1<<7,
+//            1<<8,
+//            1<<9,
+//            1<<10,
+//            1<<11,
+//            1<<12,
+//            1<<13,
+//        }, 0b10011, new int[]{1,11,1});
+        McElieceKem mck = new McElieceKem(16, 4, 2);
+        
+        boolean good;
+        do {
+            good = true;
+            System.out.println("m = " + mck.m);
+            System.out.println("n = " + mck.n);
+            System.out.println("t = " + mck.t);
+            System.out.println("q = " + mck.q);
+            System.out.println("k = " + mck.k);
+            System.out.println("g(z) = " + Arrays.toString(mck.goppaPoly));
+            System.out.println("L = " + Arrays.toString(mck.L));
+            System.out.println("f(x) = " + Integer.toBinaryString(mck.fieldPoly));
+
+            int mt = mck.m * mck.t;
+
+            int[][] h = new int[mt][mck.n];
+
+            for (int i = 0; i < mck.t; i++) {
+                for (int j = 0; j < mck.n; j++) {
+                    int hij = mck.getElementOfH(j, i);
+                    for (int k = 0; k < mck.m; k++) {
+                        h[i * mck.m + k][j] = (hij & (1 << k)) >>> k;
+                    }
+                }
+            }
+
+            System.out.println("");
+            System.out.println("H=");
+            for (int i = 0; i < mt; i++) {
+                System.out.println(Arrays.toString(h[i]));
+            }
+            System.out.println("");
+
+            for (int i = 0; i < mt; i++) {
+                if (h[i][i] == 0) {
+                    for (int j = i + 1; j < mt; j++) {
+                        if (h[j][i] == 1) {
+                            for (int z = 0; z < mck.n; z++) {
+                                h[i][z] = h[i][z] ^ h[j][z];
+                            }
+                            break;
+                        }
+                    }
+                }
+
+                for (int j = i + 1; j < mt; j++) {
+                    if (h[j][i] == 1) {
+                        for (int z = i; z < mck.n; z++) {
+                            h[j][z] = h[j][z] ^ h[i][z];
+                        }
+                    }
+                }
+
+            }
+
+            for (int i = mt - 1; i >= 1; i--) {
+                for (int j = i - 1; j >= 0; j--) {
+                    if(h[i][i] == 0){
+                        mck.L = mck.generateRandomL();
+                        mck.goppaFieldPoly = new PolynomialGF2mSmallM(mck.field, mck.t, PolynomialGF2mSmallM.RANDOM_IRREDUCIBLE_POLYNOMIAL, mck.secureRandom);
+                        good = false;
+                        continue;
+                    }
+                    if (h[j][i] == 1) {
+                        for (int z = i; z < mck.n; z++) {
+                            h[j][z] = h[j][z] ^ h[i][z];
+                        }
+                    }
+                }
+            }
+
+            int[][] g = new int[mck.k][mck.n];
+            for (int i = mt, f = 0; i < mck.n; i++, f++) {
+                for (int j = 0; j < mt; j++) {
+                    g[f][j] = h[j][i];
+                }
+            }
+            for (int i = 0; i < mck.k; i++) {
+                g[i][mt + i] = 1;
+            }
+
+            System.out.println("H^=");
+            for (int i = 0; i < mt; i++) {
+                System.out.println(Arrays.toString(h[i]));
+            }
+
+            System.out.println("");
+            System.out.println("G=");
+            for (int i = 0; i < mck.k; i++) {
+                System.out.println(Arrays.toString(g[i]));
+            }
+        } while (!good);
     }
 }
